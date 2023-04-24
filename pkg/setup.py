aee51178be171f06545be3a2a9af20937a38ebb9
George Lin <shangchl@example.com>
2023-04-24 19:07:37 +0000

    first commit

---
+++ pkg/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='s23openalex',
+      version='0.0.1',
+      description='bibtex utilities',
+      maintainer='Shang Chun Lin',
+      maintainer_email='shangchl@andrew.cmu.edu',
+      license='MIT',
+      packages=['s23openalex'],
+      scripts=[],
+      long_description='''A set of bibtex utilities''')
