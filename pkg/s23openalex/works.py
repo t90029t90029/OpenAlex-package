d933be83975b66be1654e2bec01443eff0779d28
George Lin <shangchl@example.com>
2023-04-24 19:34:57 +0000

    update works.py for black and pytest

--- pkg/s23openalex/works.py
+++ pkg/s23openalex/works.py
@@ -125,6 +125,32 @@ class Works:
         fields += ["ER  -"]
 
         ris = "\n".join(fields)
+        return ris
+
+    def ris_html(self):
+        fields = []
+        if self.data["type"] == "journal-article":
+            fields += ["TY  - JOUR"]
+        else:
+            raise Exception("Unsupported type {self.data['type']}")
+
+        for author in self.data["authorships"]:
+            fields += [f'AU  - {author["author"]["display_name"]}']
+
+        fields += [f'PY  - {self.data["publication_year"]}']
+        fields += [f'TI  - {self.data["title"]}']
+        fields += [f'JO  - {self.data["host_venue"]["display_name"]}']
+        fields += [f'VL  - {self.data["biblio"]["volume"]}']
+
+        if self.data["biblio"]["issue"]:
+            fields += [f'IS  - {self.data["biblio"]["issue"]}']
+
+        fields += [f'SP  - {self.data["biblio"]["first_page"]}']
+        fields += [f'EP  - {self.data["biblio"]["last_page"]}']
+        fields += [f'DO  - {self.data["doi"]}']
+        fields += ["ER  -"]
+
+        ris = "\n".join(fields)
         ris64 = base64.b64encode(ris.encode("utf-8")).decode("utf8")
         uri = f'<pre>{ris}<pre><br><a href="data:text/plain;base64,{ris64}" download="ris">Download RIS</a>'
         from IPython.display import HTML
